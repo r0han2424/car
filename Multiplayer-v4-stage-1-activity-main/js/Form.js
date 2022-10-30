8d15811793e79db1e65efaa27699fa7cc3cf87ad
r0han2424 <108449988+r0han2424@users.noreply.github.com>
2022-10-30 17:21:15 +0800

    Add files via upload

---
+++ Multiplayer-v4-stage-1-activity-main/js/Form.js
@@ -0,0 +1,33 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter yor name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+
+   setElementsPosition(){
+    this.titleImg.position(120, 100);
+    this.input.position(width / 2 - 90, height / 2 - 100);
+    this.playButton.position(width / 2 - 90, height / 2 - 80);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+   hide(){
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+   }
+
+
+   handleMousePressed() {
+    this.playButton.mousePressed(() =>{
+      this.input.hide();
+      this.playButton.hide();
+      var message = ` Hello ${this.input.value()} </br>wait for another player to join...`
+     
+
+    });
+  }
+}
